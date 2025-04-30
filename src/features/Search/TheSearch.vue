34852c12057c282a141d99941d479ad874fd969d
Duhno Mikhail <misha.duhno@mail.ru>
2025-05-01 02:30:30 +0300

    pagination: Добавлена пагинация и поиск по записям

---
+++ src/features/Search/TheSearch.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import InputText from "@/shared/InputText/InputText.vue";
+import {ref, watch} from "vue";
+import debounce from "@/utils/functions/debaunce.ts";
+
+const emit = defineEmits<{
+  doSearch: [string]
+}>();
+const deb = debounce();
+const searchValue = ref<string>('');
+
+watch(
+    () => searchValue.value,
+    () => {
+      deb(() => {
+        emit('doSearch', searchValue.value);
+      }, 1000);
+    }
+)
+</script>
+
+<template>
+  <div class="search-container">
+    <InputText label="Поиск" v-model="searchValue"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/widgets/PostsList/PostsList.vue
+++ src/widgets/PostsList/PostsList.vue
@@ -4,17 +4,52 @@ import {useRouter} from "vue-router";
 import {onMounted, ref} from "vue";
 import type {IPost} from "@/utils/types/postTypes.ts";
 import {getAllPosts} from "@/widgets/PostsList/api/getPosts.ts";
+import Paginator from "@/shared/Paginator";
+import Search from "@/features/Search";
 
-const posts = ref<IPost[]>([{
+const VISIBLE_ON_PAGE = 10;
+const allPosts = ref<IPost[]>([]);
+const filteredPosts = ref<IPost[]>([]);
+const visiblePosts = ref<IPost[]>([{
   title: '',
   body: '',
   id: -1,
   userId: -1
 }]);
+const pageCount = ref<number>(0);
+const currentPage = ref<number>(1);
 
-onMounted(() => {
-  getAllPosts().then(({data}) => posts.value = data);
-})
+const prepareVisiblePosts = (): void => {
+  const lastElementId = VISIBLE_ON_PAGE * currentPage.value - 1;
+  const firstElementId = lastElementId - VISIBLE_ON_PAGE + 1;
+  visiblePosts.value = filteredPosts.value.slice(firstElementId, lastElementId);
+}
+
+const getPageCount = (): void => {
+  pageCount.value = Math.ceil(filteredPosts.value.length / VISIBLE_ON_PAGE);
+}
+
+const changeCurrentPage = (page: number) => {
+  currentPage.value = page;
+  prepareVisiblePosts();
+}
+
+const filterPosts = (title: string) => {
+  if (title)
+    filteredPosts.value = allPosts.value.filter((post: IPost) => post.title.includes(title));
+  else
+    filteredPosts.value = allPosts.value;
+  getPageCount();
+  changeCurrentPage(1);
+}
+
+onMounted(async () => {
+  const { data } = await getAllPosts();
+  allPosts.value = data;
+  filteredPosts.value = allPosts.value;
+  getPageCount();
+  prepareVisiblePosts();
+});
 
 const router = useRouter();
 
@@ -28,13 +63,23 @@ const openPostPage = (recordId: number): void => {
 
 <template>
   <div class="posts-container">
-    <template v-for="post of posts" :key="post.id">
+    <div class="search">
+      <Search
+          @do-search="filterPosts"
+      />
+    </div>
+    <template v-for="post of visiblePosts" :key="post.id">
       <ThePost
           :title="post.title"
           :body="post.body"
           @click="() => openPostPage(post.id)"
       />
     </template>
+    <Paginator
+        :current-page="currentPage"
+        :count="pageCount"
+        @page-selection="changeCurrentPage"
+    />
   </div>
 </template>
 
@@ -45,5 +90,10 @@ const openPostPage = (recordId: number): void => {
   gap: 15px;
   width: 100%;
   align-items: center;
+  margin-bottom: 30px;
+}
+
+.search {
+  width: 50%;
 }
 </style>
